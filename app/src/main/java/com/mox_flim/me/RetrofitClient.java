2e48b0431d2ea806faa3814f4b3eb83783fec7eb
BD-TEEM <mrazad991@gmail.com>
2026-07-02 16:17:25 +0600

    Update RetrofitClient.java

--- app/src/main/java/com/mox_flim/me/RetrofitClient.java
+++ app/src/main/java/com/mox_flim/me/RetrofitClient.java
@@ -4,8 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    // এখানে আপনার আসল API এর Base URL দিতে হবে (যেমন TMDB বা আপনার নিজস্ব সার্ভার)
-    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+    private static final String BASE_URL = "https://iptv-org.github.io/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
@@ -18,4 +17,3 @@ public class RetrofitClient {
         return retrofit;
     }
 }
-
