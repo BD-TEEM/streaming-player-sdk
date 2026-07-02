848a73ea839640320ef6894274eda76122cf27f3
BD-TEEM <mrazad991@gmail.com>
2026-07-02 16:18:30 +0600

    Update ApiService.java

--- app/src/main/java/com/mox_flim/me/ApiService.java
+++ app/src/main/java/com/mox_flim/me/ApiService.java
@@ -5,8 +5,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface ApiService {
-    // আপনার নিজস্ব m3u/m3u8 এপিআই লিংক (উদাহরণস্বরূপ: "playlist.json")
-    @GET("path/to/your/m3u/api.json")
+    @GET("api/channels.json")
     Call<List<M3uSource>> getStreamingSources();
 }
-
